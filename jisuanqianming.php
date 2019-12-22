3f3db364bca63b53b7868b96fb82cf5241ca37b3
冰彦糖 <44092516+LeagueJhin@users.noreply.github.com>
2019-12-22 15:11:30 +0800

    新增注释方便更改
    每一行增加注释方便更改

--- jisuanqianming.php
+++ jisuanqianming.php
@@ -1,10 +1,17 @@
 <?php
-    $id=file_get_contents("getqianming_deln.txt");
-    $secretKey='￥API的Key￥';#补全Key
-    echo $id;
-    $signStr=base64_encode(hash_hmac('sha1', $id, $secretKey, true));
-    $zuizongqianming= rawurlencode($signStr);
-    $myfile=fopen("qianming.txt", "w");
-    fwrite($myfile, $signStr);
-    fclose($myfile);
+/*
+ * @Author: your name
+ * @Date: 2019-12-21 21:28:57
+ * @LastEditTime : 2019-12-22 15:04:29
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \undefinedd:\Github\-IPV6-\jisuanqianming.php
+ */
+    $id=file_get_contents("getqianming_deln.txt");          //读取文件
+    $secretKey='￥API的Key￥';#补全Key    
+    $signStr=base64_encode(hash_hmac('sha1', $id, $secretKey, true));//计算签名
+    $zuizongqianming= rawurlencode($signStr);               //encode编码    
+    $myfile=fopen("qianming.txt", "w");                     //写入文件
+    fwrite($myfile, $signStr);                              //写入文件
+    fclose($myfile);                                        //关闭写入文件流
 ?>
--- test.sh
+++ test.sh
@@ -3,23 +3,24 @@
  # @Version: 0.1.1
  # @Autor: 冰彦糖
  # @Date: 2019-11-28 22:23:50
- # @LastEditors: Please set LastEditors
- # @LastEditTime: 2019-12-21 21:11:11
+ # @LastEditors  : Please set LastEditors
+ # @LastEditTime : 2019-12-22 15:02:54
  ###
-curl -v "http://ip.gae.zxinc.org/info.php?type=json" > yuanIP.txt;
-grep -Po 'myip[" :]+\K[^"]+' yuanIP.txt>IP.txt;
-read IP < IP.txt;
-unixtime=`date '+%s'`;
-# 补全pinjie
+curl -v "http://ip.gae.zxinc.org/info.php?type=json" > yuanIP.txt;  #从API处获取IP地址写入yuanIP.txt
+grep -Po 'myip[" :]+\K[^"]+' yuanIP.txt>IP.txt;                     #使用正则表达式获取IP
+read IP < IP.txt;                                                   #读取IP.txt存入名为IP的变量
+unixtime=`date '+%s'`;                                              #UNIX时间
+# 补全pinjie                                                        
 pinjie="cns.api.qcloud.com/v2/index.php?Action=RecordModify&Nonce="${RANDOM}"&Region=&SecretId=￥API的ID￥&Timestamp="${unixtime}"&domain=￥域名￥&recordId=￥解析记录的ID￥&recordLine=默认&recordType=AAAA&subDomain=￥子域名￥&value="${IP};
-echo "GET"$pinjie>getqianming.txt;
-awk '{printf("%s",$0)}' getqianming.txt>getqianming_deln.txt;
-/usr/bin/php jisuanqianming.php
-sleep 3s;
-read qianming<qianming.txt;
-qingqiu="https://"${pinjie}"&Signature="${qianming};
-echo $qingqiu>zuizhong.txt;
-curl $qingqiu;
+#需要填入域名，解析记录的ID(可以用腾讯云API在线调试v2 域名查询获取)，或直接更改此pinjie字符串查询，子域名
+echo "GET"$pinjie>getqianming.txt;                                  #拼接写入getqianming.txt用于php签名
+awk '{printf("%s",$0)}' getqianming.txt>getqianming_deln.txt;       #去除直接<获取时linux导致莫名回车问题
+/usr/bin/php jisuanqianming.php                                     #调用jisuanqianming.php
+sleep 3s;                                                           #等待计算
+read qianming<qianming.txt;                                         #读取签名
+qingqiu="https://"${pinjie}"&Signature="${qianming};                #拼接字符串
+echo $qingqiu>zuizhong.txt;                                         #写入文件课查询错误使用【可选】
+curl $qingqiu;                                                      #请求更改解析   
 
 
 
